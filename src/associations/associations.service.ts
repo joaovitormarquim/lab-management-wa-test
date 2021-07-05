72e4cc2f68194ebd6a7007caadd86b629e89a2e8
João Vítor Marquim <jvmarquim@gmail.com>
2021-07-05 18:41:05 -0300

    Added verification if the exam and lab are active before association

--- src/associations/associations.service.ts
+++ src/associations/associations.service.ts
@@ -5,6 +5,9 @@ import { AssociationsRepository } from './associations.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExamsRepository } from 'src/exams/exams.repository';
 import { LabsRepository } from 'src/labs/labs.repository';
+import { ExamStatus } from 'src/exams/enum/exam-status.enum';
+import { UnprocessableEntityException } from '@nestjs/common';
+import { LabStatus } from 'src/labs/enum/lab-status.enum';
 
 @Injectable()
 export class AssociationsService {
@@ -19,13 +22,23 @@ export class AssociationsService {
     createAssociationDto: CreateAssociationDto,
   ): Promise<Association> {
     const { exam_id, lab_id } = createAssociationDto;
-    const exam = this.examsRepository.findOne(exam_id);
+    const exam = await this.examsRepository.findOne(exam_id);
     if (!exam) {
       throw new NotFoundException(`Exam with id '${exam_id}' not found`);
     }
-    const lab = this.labsRepository.findOne(lab_id);
+    if (exam.status !== ExamStatus.ACTIVE) {
+      throw new UnprocessableEntityException(
+        'The exam must be active to be associated with a lab',
+      );
+    }
+    const lab = await this.labsRepository.findOne(lab_id);
     if (!lab) {
       throw new NotFoundException(`Lab with id '${lab_id}' not found`);
+    }
+    if (lab.status !== LabStatus.ACTIVE) {
+      throw new UnprocessableEntityException(
+        'The lab must be active to be associated with an exam',
+      );
     }
     const association = this.associationsRepository.create({ exam_id, lab_id });
     await this.associationsRepository.save(association);
